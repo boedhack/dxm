d726091a4ec9ee8e13ae1528591527bdbc97ee30
dianlujitao <dianlujitao@lineageos.org>
2017-12-07 22:15:14 +0800

    mojito: Build dummy android.hidl.manager@1.0 [daniml3]: convert to blueprint
    Change-Id: I81a081130869c4d92f30b2674e75bb6b096a7a2c
    Signed-off-by: boedhack99 <boedhack@gmail.com>

--- device.mk
+++ device.mk
@@ -260,7 +260,8 @@ PRODUCT_PACKAGES += \
 # HIDL
 PRODUCT_PACKAGES += \
     android.hidl.base@1.0 \
-    android.hidl.base@1.0.vendor
+    android.hidl.base@1.0.vendor \
+    android.hidl.manager@1.0
 
 # IDC
 PRODUCT_COPY_FILES += \
