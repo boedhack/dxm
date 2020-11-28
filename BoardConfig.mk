85ed9cb1b710160476c0def0ef3683b66bdabfdf
Arian <arian.kulmer@web.de>
2020-11-28 18:49:15 +0100

    mojito: Do not explicitly include qcom sepolicy Change-Id: I9cf2081062f4412f602a42d5e9e225422868e83f
    Signed-off-by: boedhack99 <boedhack@gmail.com>

--- BoardConfig.mk
+++ BoardConfig.mk
@@ -203,7 +203,6 @@ VENDOR_SECURITY_PATCH := 2021-07-01
 
 # Sepolicy
 TARGET_SEPOLICY_DIR := msmsteppe
-include device/qcom/sepolicy_vndr/SEPolicy.mk
 BOARD_PLAT_PRIVATE_SEPOLICY_DIR += $(DEVICE_PATH)/sepolicy/private
 BOARD_PLAT_PUBLIC_SEPOLICY_DIR += $(DEVICE_PATH)/sepolicy/public
 BOARD_VENDOR_SEPOLICY_DIRS += $(DEVICE_PATH)/sepolicy/vendor
