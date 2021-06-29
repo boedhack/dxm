11e8fbde27828e08d5169aeac231aec045b31327
raghavt20 <raghavtandon2000@gmail.com>
2021-06-30 03:48:51 +0530

    mojito: Drop mediacodec seccomp policy
    - We already have it in device/qcom/common/common.mk
    
    Change-Id: Iea1dc508878684a435cc58aa62c8275b80426569
    Signed-off-by: boedhack99 <boedhack@gmail.com>

--- device.mk
+++ device.mk
@@ -303,7 +303,6 @@ PRODUCT_COPY_FILES += \
     frameworks/av/media/libstagefright/data/media_codecs_google_c2_video.xml:$(TARGET_COPY_OUT_VENDOR)/etc/media_codecs_google_c2_video.xml
 
 PRODUCT_COPY_FILES += \
-    hardware/qcom/media/conf_files/sm6150/mediacodec-seccomp.policy:$(TARGET_COPY_OUT_VENDOR)/etc/seccomp_policy/mediacodec.policy \
     hardware/qcom/media/conf_files/sm6150/system_properties.xml:$(TARGET_COPY_OUT_VENDOR)/etc/system_properties.xml
 
 # Network
