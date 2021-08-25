1e6b5ebd171790ef247717191bb80b1d3485f73c
boedhack99 <boedhack@gmail.com>
2021-08-26 01:54:11 +0700

    mojito: Add LD.lld linker * Needed by Legion-X
    Signed-off-by: boedhack99 <boedhack@gmail.com>

--- BoardConfig.mk
+++ BoardConfig.mk
@@ -135,6 +135,7 @@ BOARD_KERNEL_CMDLINE += swiotlb=1
 BOARD_KERNEL_CMDLINE += msm_rtb.filter=0x237
 BOARD_KERNEL_CMDLINE += video=vfb:640x400,bpp=32,memsize=3072000
 
+KERNEL_LD := LD=ld.lld
 KERNEL_TOOLCHAIN := $(shell pwd)/prebuilts/clang/host/linux-x86/clang-proton/bin
 TARGET_KERNEL_CROSS_COMPILE_PREFIX := aarch64-linux-gnu-
 TARGET_KERNEL_CLANG_VERSION := proton
