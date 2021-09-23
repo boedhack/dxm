2ce0c8212dbcdb10b8befc2a00b594eb9028e7c9
boedhack99 <boedhack@gmail.com>
2021-09-23 23:03:40 +0700

    mojito: Don't build libhwbinder
    Signed-off-by: boedhack99 <boedhack@gmail.com>

--- device.mk
+++ device.mk
@@ -260,8 +260,7 @@ PRODUCT_PACKAGES += \
 # HIDL
 PRODUCT_PACKAGES += \
     android.hidl.base@1.0 \
-    android.hidl.base@1.0.vendor \
-    libhwbinder.vendor
+    android.hidl.base@1.0.vendor
 
 # IDC
 PRODUCT_COPY_FILES += \
