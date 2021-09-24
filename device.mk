9136fdc79f4fb9a1ad53987617b7037c5eb1d5dd
boedhack99 <boedhack@gmail.com>
2021-09-24 15:30:53 +0700

    Revert: mojito: Build dummy android.hidl.manager@1.0 Also libhidl.base
    Signed-off-by: boedhack99 <boedhack@gmail.com>

--- device.mk
+++ device.mk
@@ -237,9 +237,6 @@ PRODUCT_PACKAGES += \
 
 # HIDL
 PRODUCT_PACKAGES += \
-    android.hidl.base@1.0 \
-    android.hidl.base@1.0.vendor \
-    android.hidl.manager@1.0
 
 # IDC
 PRODUCT_COPY_FILES += \
