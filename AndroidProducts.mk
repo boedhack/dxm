761d353a4dcfeabae27c2ebbd898794ec43255f0
boedhack99 <boedhack@gmail.com>
2022-01-20 13:07:27 +0700

    mojito: Arrow-Ify

--- AndroidProducts.mk
+++ AndroidProducts.mk
@@ -5,9 +5,9 @@
 #
 
 PRODUCT_MAKEFILES := \
-    $(LOCAL_DIR)/havoc_mojito.mk
+    $(LOCAL_DIR)/arrow_mojito.mk
 
 COMMON_LUNCH_CHOICES := \
-    havoc_mojito-user \
-    havoc_mojito-userdebug \
-    havoc_mojito-eng
+    arrow_mojito-user \
+    arrow_mojito-userdebug \
+    arrow_mojito-eng
--- arrow_mojito.mk
+++ arrow_mojito.mk
@@ -11,20 +11,16 @@ $(call inherit-product, $(SRC_TARGET_DIR)/product/full_base_telephony.mk)
 # Inherit from mojito device
 $(call inherit-product, device/xiaomi/mojito/device.mk)
 
-# Inherit some common Havoc stuff
+# Inherit some common Arrow stuff
 TARGET_BOOT_ANIMATION_RES := 1080
 TARGET_INCLUDE_LIVE_WALLPAPERS := false
-$(call inherit-product, vendor/havoc/config/common_full_phone.mk)
+$(call inherit-product, vendor/arrow/config/common.mk)
 
 # Device identifier. This must come after all inclusions.
-PRODUCT_NAME := havoc_mojito
+PRODUCT_NAME := arrow_mojito
 PRODUCT_DEVICE := mojito
-PRODUCT_BRAND := Redmi
+PRODUCT_BRAND := Xiaomi
 PRODUCT_MODEL := Redmi Note 10
 PRODUCT_MANUFACTURER := Xiaomi
 
 PRODUCT_GMS_CLIENTID_BASE := android-xiaomi
-
-WITH_GAPPS := true
-TARGET_GAPPS_ARCH := arm64
-IS_PHONE := true
